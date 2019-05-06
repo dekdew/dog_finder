e21dadbfac7e5d7043bbad691b1e6b56a2bd2cbe
dekdew <passawit1999@gmail.com>
2019-05-07 00:42:42 +0700

    link breed field

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,57 +2,62 @@ import datetime
 
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.forms import ModelChoiceField
 from django.contrib.auth.models import User
 
 from accounts.models import Profile, Dog, Breed
 
 
 class UserRegisterForms(UserCreationForm):
-    email = forms.EmailField()
+  email = forms.EmailField()
 
-    class Meta:
-        model = User
-        fields = ['username', 'email']
+  class Meta:
+    model = User
+    fields = ['username', 'email']
 
 
 class UserUpdateForms(forms.ModelForm):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=255)
-    last_name = forms.CharField(max_length=255)
+  email = forms.EmailField()
+  first_name = forms.CharField(max_length=255)
+  last_name = forms.CharField(max_length=255)
 
-    class Meta:
-        model = User
-        fields = ['email', 'first_name', 'last_name']
+  class Meta:
+    model = User
+    fields = ['email', 'first_name', 'last_name']
 
 
 class ProfileUpdateForm(forms.ModelForm):
-    owner_phone = forms.CharField(max_length=20)
-    owner_address = forms.Textarea()
+  owner_phone = forms.CharField(max_length=20)
+  owner_address = forms.Textarea()
 
-    class Meta:
-        model = Profile
-        fields = ['owner_phone', 'owner_address', 'image']
+  class Meta:
+    model = Profile
+    fields = ['owner_phone', 'owner_address', 'image']
+
+
+class BreedModelChoiceField(ModelChoiceField):
+  def label_from_instance(self, obj):
+    return "%s" % obj.breed_name
 
 
 class DogRegisterForms(forms.ModelForm):
-    dog_name = forms.CharField(max_length=50)
-    dog_gender = forms.ChoiceField(choices=Dog.GENDER)
-    dog_dob = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
-    dog_age = forms.IntegerField()
-    dog_color = forms.CharField(max_length=50)
-    dog_breed = forms.ChoiceField(choices=Breed.BREED)
-    dog_status = forms.ChoiceField(choices=Dog.STATUS)
-    dog_info = forms.CharField(widget=forms.Textarea)
+  dog_name = forms.CharField(max_length=50)
+  dog_gender = forms.ChoiceField(choices=Dog.GENDER)
+  dog_dob = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
+  dog_age = forms.IntegerField()
+  dog_color = forms.CharField(max_length=50)
+  dog_breed = BreedModelChoiceField(queryset=Breed.objects.all(), to_field_name="breed_name")
+  dog_status = forms.ChoiceField(choices=Dog.STATUS)
+  dog_info = forms.CharField(widget=forms.Textarea)
 
+  class Meta:
+    model = Dog
+    fields = ['dog_image', 'dog_name', 'dog_gender', 'dog_breed', 'dog_dob', 'dog_age', 'dog_status', 'dog_info']
 
-    class Meta:
-        model = Dog
-        fields = ['dog_image','dog_name', 'dog_gender', 'dog_breed', 'dog_dob', 'dog_age', 'dog_status', 'dog_info']
+  def clean_dog_dob(self):
+    data = self.cleaned_data['dog_dob']
+    if data > datetime.date.today():
+      raise forms.ValidationError('วันเกิิดสุนัขของท่านไม่ใช่วันในอดีต โปรดตรวจสอบวันเกิดสุนัขอีกครั้ง')
 
-    def clean_dog_dob(self):
-        data = self.cleaned_data['dog_dob']
-        if data > datetime.date.today():
-            raise forms.ValidationError('วันเกิิดสุนัขของท่านไม่ใช่วันในอดีต โปรดตรวจสอบวันเกิดสุนัขอีกครั้ง')
-
-        print(data)
-        return data
+    print(data)
+    return data
